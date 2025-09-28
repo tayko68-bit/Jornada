4374e3454a67a4bf80b84294c44ca215b1041384
tayko68-bit <tayko68@gmail.com>
2025-09-28 08:57:03 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,24 @@
 const CACHE_NAME = 'jornada-cache-v1';
 const urlsToCache = [
   './jornada_gps.html',
-  // Si tienes una hoja de estilos CSS externa, añádela aquí
+  'https://cdn.jsdelivr.net/npm/qr-scanner@1.4.2/qr-scanner.umd.min.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
